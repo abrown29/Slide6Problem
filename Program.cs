2fb4058222e5220fac3935412b18660efc0b985f
Amanda Brown <amanda.brown@gmail.com>
2016-09-14 16:37:41 -0400

    slide 6 problem done

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slide6Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int userNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(Double(userNumber));
+        }
+
+        static int Double(int userNumber)
+        {
+            int doubledInt = userNumber * 2;
+            return doubledInt;
+        }
+    }
+}
